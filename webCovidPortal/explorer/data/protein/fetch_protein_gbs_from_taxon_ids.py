e00dbc7722756132f0e055271cd629c7a5d0e4c8
Nic Webb <nhydrazine@gmail.com>
2020-06-10 21:50:55 -0400

    updating sequences and sequence fetcher

--- webCovidPortal/explorer/data/protein/fetch_protein_gbs_from_taxon_ids.py
+++ webCovidPortal/explorer/data/protein/fetch_protein_gbs_from_taxon_ids.py
@@ -1,4 +1,4 @@
-import sys, time;
+import sys, time, os;
 import numpy as np, pandas as pd;
 import urllib, requests, xmltodict, pprint;
 ################################################################################
@@ -9,7 +9,7 @@ failfile = "./failed_fetch_taxa.csv"; # taxon_ids whose protein fetch failed
 def fetch_protein_ids_from_taxon_id(taxon_id, protein_name="spike"):
     base = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?";
     options = {
-        'term'          : "txid"+taxon_id+"[Oraganism:noexp] AND "+protein_name,
+        'term'          : "txid"+taxon_id+"[Organism:noexp] AND "+protein_name,
         'db'            : "protein",
         'usehistory'    : "n",
     }
@@ -24,7 +24,7 @@ def fetch_protein_gbs_from_taxon_id(taxon_id, protein_name="spike", retmax=500):
     base_search = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?";
     base_fetch = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?";
     search_options = {
-        'term'          : "txid"+taxon_id+"[Oraganism:noexp] AND "+protein_name,
+        'term'          : "txid"+taxon_id+"[Organism:noexp] AND "+protein_name,
         'db'            : "protein",
         'usehistory'    : "y",
     };
@@ -46,6 +46,7 @@ def fetch_protein_gbs_from_taxon_id(taxon_id, protein_name="spike", retmax=500):
             fetch_url = base_fetch + urllib.parse.urlencode(fetch_options);
             gbs = requests.get(fetch_url).text;
             buffer.append(gbs);
+            print('.', end='', flush=True);
     return buffer;
 
 
@@ -57,20 +58,25 @@ taxon_ids = df['gb_taxon_id'][
     (df['pcount']>0)
 ].astype(str);
 
-fh = open(outfile, "w");
+# fh = open(outfile, "w");
 for taxon_id in taxon_ids:
     print("Downloading for "+taxon_id+" ... ", end='', flush=True);
+    filename = os.path.join('genbank_files',taxon_id+'.fna');
+    if os.path.isfile(filename):
+        print("File exists.");
+        continue;
     try:
         gbs = fetch_protein_gbs_from_taxon_id(taxon_id);
-        print("OK");
-        fh.write("".join(gbs));
-    except:
-        print("Failed "+taxon_id);
+        with open(filename,'w') as fh:
+            fh.write("".join(gbs));
+        print("--> "+filename+" ("+str(len(gbs))+")");
+        # fh.write("".join(gbs));
+    except Exception as e:
+        print("Failed "+taxon_id+": "+str(e));
         failed_ids.append(taxon_id);
-        fh.close();
-        sys.exit();
+        continue;
     time.sleep(1);
-fh.close();
+# fh.close();
 print("Wrote to "+outfile);
 failed_ids = pd.DataFrame({'taxon_id': failed_ids});
 print("Writing "+failfile);
