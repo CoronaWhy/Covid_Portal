b358c00fd8e1e61f059c67efac30bf216fcad8b3
Nic Webb <nhydrazine@gmail.com>
2020-06-14 19:18:07 -0400

    pipeline settings

--- webCovidPortal/explorer/data/protein/settings.py
+++ webCovidPortal/explorer/data/protein/settings.py
@@ -3,15 +3,18 @@ import os;
 genbank_root = os.path.join('genbank_files');
 alignment_root = os.path.join('alignments');
 
-
-
-
-
-
 # folder names
 taxon_prealign_dir = 'taxon_prealign';
 taxon_aligned_dir = 'taxon_aligned';
 ngap_dir = 'ngap_consensus';
+
+
+
+
+
+
 # file basenames
 ngap_prealign_fbase = 'ngap_prealign';
 ngap_aligned_fbase = 'ngap_aligned';
+
+#
